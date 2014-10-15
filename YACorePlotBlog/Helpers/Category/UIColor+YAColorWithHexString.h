c21610b52806819d192cb518dfc124d9a80d235f
Eugene Goloboyar <eugene.goloboyar@yalantis.com>
2014-10-15 16:47:55 +0300

    [BarChartView] - added category for color with string

---
+++ YACorePlotBlog/Helpers/Category/UIColor+YAColorWithHexString.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+YAColorWithHexString.h
+//  CorePlotBlog
+//
+//  Created by Евгений on 13.10.14.
+//  Copyright (c) 2014 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (YAColorWithHexString)
+
++ (UIColor *)ya_colorFromHexString:(NSString *)hexString;
+
+@end
