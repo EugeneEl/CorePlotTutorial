74de6eddd13fc6f1708824c2b2b4d551aaecfb8f
Eugene Goloboyar <eugene.goloboyar@yalantis.com>
2014-10-15 15:28:26 +0300

    Initial Commit

---
+++ YACorePlotBlog/YAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YAAppDelegate.h
+//  YACorePlotBlog
+//
+//  Created by Евгений on 15.10.14.
+//  Copyright (c) 2014 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YACorePlotBlog/YAViewController.h
@@ -0,0 +1,13 @@
+//
+//  YAViewController.h
+//  YACorePlotBlog
+//
+//  Created by Евгений on 15.10.14.
+//  Copyright (c) 2014 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YAViewController : UIViewController
+
+@end
