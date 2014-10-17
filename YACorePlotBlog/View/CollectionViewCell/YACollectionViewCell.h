02cf105b31cf1b5d5e89b5702ab9a0aab99a36a8
Eugene Goloboyar <eugene.goloboyar@yalantis.com>
2014-10-17 16:14:06 +0300

    added collection view

---
+++ YACorePlotBlog/View/CollectionViewCell/YACollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  YACollectionViewCell.h
+//  YACorePlotBlog
+//
+//  Created by Евгений on 17.10.14.
+//  Copyright (c) 2014 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YACollectionViewCell : UICollectionViewCell
+
+@end
